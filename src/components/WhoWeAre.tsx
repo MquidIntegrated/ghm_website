71a5e6d1b081d1eb78c8a13ab23d289bf10822ac
Jaachi Okafor <okaforjaachi@gmail.com>
2025-02-27 12:35:42 +0100

    fix: updated company description and improve clarity in WhoWeAre component

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -13,20 +13,20 @@ const WhoWeAre = () => {
           Who We Are
         </h4>
         <h2 className="text-ghmBlack font-semibold text-3xl md:text-4xl max-w-lg mb-10">
-          We are a dynamic and innovative health insurance provider
+          We are a dynamic and innovative health insurance company
         </h2>
         <p className="text-ghmGrey-500 mb-5 md:mb-10 max-w-3xl">
+          Established in 2021,{" "}
           <span className="font-semibold">
             Grooming Health Management Limited (GHML)
           </span>{" "}
-          was established in the year 2021 and is proudly registered with the
-          Corporate Affairs Commission (CAC) under{" "}
-          <span className="font-semibold">certificate number 1771536.</span> Our
-          organization operates as a nationally recognized{" "}
-          <span className="font-semibold">
-            Health Maintenance Organization (HMO)
-          </span>{" "}
-          , serving as a pivotal entity in the Health Insurance landscape.
+          is a nationally recognized Health Maintenance Organization (HMO)
+          registered with the Corporate Affairs Commission (CAC) under{" "}
+          <span className="font-semibold">certificate number 1771536.</span> As
+          a key player in the health insurance sector, GHML is committed to
+          providing comprehensive and reliable healthcare solutions, ensuring
+          quality and accessible medical services for individuals and
+          organizations nationwide.
         </p>
         <p className="text-ghmPurple-300 ">
           That&apos;s why we go beyond traditional Health Insurance models to
